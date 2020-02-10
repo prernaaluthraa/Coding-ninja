36973680295983109baacb6641c7ebbb52917174
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2020-02-10 13:26:17 +0530

    Add files via upload

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,5 +1,32 @@
 package yayyy;
-
+import java.util.*;
 public class SelectionSort {
-
+              public static void selectionsort(int[] input) {
+            	  for(int i=0;i<input.length-1;i++)//for no.of rounds {
+            	  {
+            		  int min=input[i];
+            		  int minIndex=i;
+            		  for(int j=i+1;j<input.length;j++) {
+            			  if(input[j]<min) {
+            				  min=input[j];
+            				  minIndex=j;
+            			  }
+            		  }
+            		  //now swap
+            		  if(minIndex !=i) {
+            		  input [minIndex]=input[i];
+            		  input[i]=min;
+            		  
+            	      }
+                   }
+              }
+	public static void main(String [] args) {
+            	  int input[]= {2,6,9,1,5};
+            	  selectionsort(input);
+            	  for(int i=0;i<input.length;i++) {
+            		  System.out.println(input[i]+" ");
+            	  }
+            	
+            	  
+            	   }
 }
