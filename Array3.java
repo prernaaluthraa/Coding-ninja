a702ddad448e93984c62f791e0a49a86d754340e
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-05-21 17:40:18 -0400

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -1,37 +1,34 @@
-package yayyy;
+    try:
+        hardship_toggle = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div/div/main/div/div[2]/div[2]/div/div[2]/div/div/div/div/div[3]/div[2]/div/div/fieldset/form/div[4]/table/div/div[2]/div[3]/div/div[2]/label[2]/span')))
+        hardship_toggle_color=hardship_toggle.value_of_css_property('background-color')
+        hardship_toggle_color=hardship_toggle_color.replace('rgba','')
+        hardship_toggle_color=eval(hardship_toggle_color)
+        hardship_toggle_hex='#{:02x}{:02x}{:02x}'.format(*hardship_toggle_color)
 
-import java.util.Scanner;
+        if (str(obj.hardship_resolved)).lower() == "yes":
+            
+            if  obj.bluergbcolor in hardship_toggle_hex:
+                print("hardship is already yes")
+                obj.printlog.info(f"Hardship toggle already selected. input:{obj.hardship_resolved} 4.3")        
+               
+            else:
+                hardship_toggle.click()
+                obj.printlog.info(f"Hardship toggle selected. input:{obj.hardship_resolved} 4.3")
+           
+        elif (str(obj.hardship_resolved)).lower() == "no":
 
-public class Array3 {
-	
-		public static int searchIndex(int x) {
-			int n= arr.length;
-			for(int i = 0; i < n; i++) 
-		    { 
-		        if(arr[i] == x) 
-		            return i; 
-		    } 
-		    return -1; 
-		}
-        public static int[] takeInputss()
-        {
-        	Scanner s=new Scanner(system.in);
-        	int size= s.nextInt();
-        	int arr[]=new int[size];
-        	
-        	for(int i=0;i<size;i++)
-        	{
-        		arr[i]=s.nextInt();
-        		
-        	}
-        }
+            if  obj.bluergbcolor in hardship_toggle_hex:
+                
+                hardship_toggle.click()
+                obj.printlog.info(f"Hardship toggle unselected. input:{obj.hardship_resolved} 4.3")
+            else:
+                print("hardship already toggle no")
+                obj.printlog.info(f"Hardship toggle  already unselected. input:{obj.hardship_resolved} 4.3")
+        obj.printlog.info("hardship toggle answered. 4.3")
+        obj.driver.save_screenshot(obj.config['SS']+loanNumber+"FHA"+"Hardshiptoggle_4_3.png")
+    except (TimeoutException, StaleElementReferenceException) as e:
+        print(f"Error selecting '{obj.hardship_resolved}' toggle hardship: {e}")
         
-        public static main(Strint[] args) {
+    except Exception as ex:
+        raise Exception(f"Hardship toggle {obj.hardship_resolved} not unanswered.4.3", str(ex))    
 
-            int result = searchIndex(x); 
-            if(result == -1) 
-                System.out.print("Element is not present in array"); 
-            else
-                System.out.print("Element is present at index " + result);
-        }
-}
