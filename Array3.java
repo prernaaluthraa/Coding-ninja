1b5116969639afc75b41a93dc76dcbaeadadeea3
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-05-22 17:27:50 -0400

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -1,34 +1,10 @@
-    try:
-        hardship_toggle = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div/div/main/div/div[2]/div[2]/div/div[2]/div/div/div/div/div[3]/div[2]/div/div/fieldset/form/div[4]/table/div/div[2]/div[3]/div/div[2]/label[2]/span')))
-        hardship_toggle_color=hardship_toggle.value_of_css_property('background-color')
-        hardship_toggle_color=hardship_toggle_color.replace('rgba','')
-        hardship_toggle_color=eval(hardship_toggle_color)
-        hardship_toggle_hex='#{:02x}{:02x}{:02x}'.format(*hardship_toggle_color)
-
-        if (str(obj.hardship_resolved)).lower() == "yes":
-            
-            if  obj.bluergbcolor in hardship_toggle_hex:
-                print("hardship is already yes")
-                obj.printlog.info(f"Hardship toggle already selected. input:{obj.hardship_resolved} 4.3")        
-               
-            else:
-                hardship_toggle.click()
-                obj.printlog.info(f"Hardship toggle selected. input:{obj.hardship_resolved} 4.3")
-           
-        elif (str(obj.hardship_resolved)).lower() == "no":
-
-            if  obj.bluergbcolor in hardship_toggle_hex:
-                
-                hardship_toggle.click()
-                obj.printlog.info(f"Hardship toggle unselected. input:{obj.hardship_resolved} 4.3")
-            else:
-                print("hardship already toggle no")
-                obj.printlog.info(f"Hardship toggle  already unselected. input:{obj.hardship_resolved} 4.3")
-        obj.printlog.info("hardship toggle answered. 4.3")
-        obj.driver.save_screenshot(obj.config['SS']+loanNumber+"FHA"+"Hardshiptoggle_4_3.png")
-    except (TimeoutException, StaleElementReferenceException) as e:
-        print(f"Error selecting '{obj.hardship_resolved}' toggle hardship: {e}")
-        
-    except Exception as ex:
-        raise Exception(f"Hardship toggle {obj.hardship_resolved} not unanswered.4.3", str(ex))    
-
+Traceback (most recent call last):
+  File "D:\RPA\Prerna\Investor Rules\InvestorRulesdevDB.py", line 37, in <module>
+    df = pd.DataFrame(results, columns=column_names)
+  File "C:\Users\luthrapr\AppData\Roaming\Python\Python39\site-packages\pandas\core\frame.py", line 798, in __init__
+    mgr = ndarray_to_mgr(
+  File "C:\Users\luthrapr\AppData\Roaming\Python\Python39\site-packages\pandas\core\internals\construction.py", line 337, in ndarray_to_mgr
+    _check_values_indices_shape_match(values, index, columns)
+  File "C:\Users\luthrapr\AppData\Roaming\Python\Python39\site-packages\pandas\core\internals\construction.py", line 408, in _check_values_indices_shape_match
+    raise ValueError(f"Shape of passed values is {passed}, indices imply {implied}")
+ValueError: Shape of passed values is (17, 1), indices imply (17, 51)
