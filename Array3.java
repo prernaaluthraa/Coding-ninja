0d774785a4c949083f5f1629e2daccf498f75c13
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-05-23 03:04:46 +0530

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -1,12 +1,26 @@
-Traceback (most recent call last):
-  File "D:\RPA\Prerna\Investor Rules\InvestorRulesdevDB.py", line 39, in <module>
-    df = pd.DataFrame(results_transposed, columns=column_names)
-  File "C:\Users\luthrapr\AppData\Roaming\Python\Python39\site-packages\pandas\core\frame.py", line 782, in __init__
-    arrays, columns, index = nested_data_to_arrays(
-  File "C:\Users\luthrapr\AppData\Roaming\Python\Python39\site-packages\pandas\core\internals\construction.py", line 498, in nested_data_to_arrays
-    arrays, columns = to_arrays(data, columns, dtype=dtype)
-  File "C:\Users\luthrapr\AppData\Roaming\Python\Python39\site-packages\pandas\core\internals\construction.py", line 840, in to_arrays
-    content, columns = _finalize_columns_and_data(arr, columns, dtype)
-  File "C:\Users\luthrapr\AppData\Roaming\Python\Python39\site-packages\pandas\core\internals\construction.py", line 937, in _finalize_columns_and_data
-    raise ValueError(err) from err
-ValueError: 51 columns passed, passed data had 17 columns
+
+# Establish the database connection
+cnxn = pyodbc.connect(con_string)
+cursor = cnxn.cursor()
+
+# Execute the query
+cursor.execute(query)
+
+# Get the column names
+column_names = [column[0] for column in cursor.description]
+
+# Fetch all the results
+results = cursor.fetchall()
+
+# Create a DataFrame from the results and column names
+df = pd.DataFrame(results, columns=column_names)
+
+# Save the DataFrame to an Excel file
+output_file_path = '/application/Output.xlsx'
+df.to_excel(output_file_path, index=False)
+
+# Close the cursor and connection
+cursor.close()
+cnxn.close()
+
+print(f"Results saved to '{output_file_path}'")
