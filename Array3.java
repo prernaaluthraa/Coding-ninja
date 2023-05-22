5c489ff54f26693323220e57c25949e3cfa863b0
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-05-22 17:44:52 -0400

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -1,7 +1,6 @@
-Number of columns in results 51
-Number of column names 51
+('U8Y', 'tytytytyty', 'ANY', 'YES', 'YES', 'NO', 'NO', 'YES', 'YES', 'YES', 'YES', 'YES', 'NO', 'NO', 'YES', 'YES', 'YES', 'YES', 'NO', 'ONLY STANDARD FREDDIE MODS ALLOWED', '.0125', None, 'PMMS', None, None, 480, None, 3, None, None, None, None, None, None, None, None, None, None, None, None, '20%', '', '11', '', None, '', '', datetime.datetime(2023, 4, 6, 11, 36), datetime.datetime(2023, 4, 6, 11, 36), datetime.datetime(2023, 4, 6, 11, 36), datetime.datetime(2023, 4, 6, 11, 36))
 Traceback (most recent call last):
-  File "D:\RPA\Prerna\Investor Rules\InvestorRulesdevDB.py", line 42, in <module>
+  File "D:\RPA\Prerna\Investor Rules\InvestorRulesdevDB.py", line 46, in <module>
     df = pd.DataFrame(results, columns=column_names)
   File "C:\Users\luthrapr\AppData\Roaming\Python\Python39\site-packages\pandas\core\frame.py", line 798, in __init__
     mgr = ndarray_to_mgr(
