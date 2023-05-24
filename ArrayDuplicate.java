ed207d41c913eefe75fa18e3ae5f29c9fb3f8365
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-05-24 03:21:35 -0400

    Update ArrayDuplicate.java

--- ArrayDuplicate.java
+++ ArrayDuplicate.java
@@ -1,12 +1,20 @@
-BOT to change the date daily while running the attached SQL query for below highlighted field . ( BOT to change the date as per last run date  )
-For Example : 
-i. BOT run the SQL query on Friday i.e. 04/14/2023 than date on below query needs to be change to 04/13/2023 ( as last SQL run date )
-ii. BOT run the SQL query on Monday i.e. 04/17/2023 than date on below query need to be change to 04/14/2023 ( as last SQL run date )
+try:
+    time.sleep(2)
+    in_floorrate = '2'
+    # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    floorrate_element = driver.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div/div[2]/div/div/div[3]/form/div[1]/div/div[2]/div/div[3]/div/div/div/div/table/tbody/tr[2]/td[2]/div/div/div/span/input')
+    floorrate_text = floorrate_element.text
+    print("Text from the floorrate field",floorrate_text)
+    if floorrate_text == in_floorrate:
+        print("floorrate in the bkfs field matched with the output")
+    else:
+        floorrate_element.send_keys(Keys.CONTROL + "a")
+        floorrate_element.send_keys(Keys.BACKSPACE)
+        floorrate_element.send_keys(in_floorrate)
+        print("floorrate_element replaced.")    
 
-Date need to be change to below as per provided SQL query
-WHERE
-	M.[ReviewedByDateTime] >= '04/01/23'
-	OR F.[ReviewedByDateTime] >= '04/01/23'
-	OR DIL.[ReviewedByDateTime] >= '04/01/23'
-	OR S.[ReviewedByDateTime] >= '04/01/23'
-Copy and paste the Attached SQL > Click on F5
+except (TimeoutException, StaleElementReferenceException) as e:
+    traceback.print_exc()           
+    print(f"error:{e}")       
+except Exception as ex:
+    raise Exception(f"error:", str(ex))
