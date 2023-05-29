1fb6741330aef9d771ebdbfa2f8777f54f04b68e
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-05-29 17:12:11 -0400

    Update ArrayAssignment1.java

--- ArrayAssignment1.java
+++ ArrayAssignment1.java
@@ -1,51 +1,102 @@
-obj.investor_id = obj.InputData['INVESTOR_ID'][obj.TransactionNumber-1]
-obj.investor_deal_name = obj.InputData['INVESTOR_DEAL_NAME'][obj.TransactionNumber-1]
-obj.investor_category = obj.InputData['INVESTOR_CATEGORY'][obj.TransactionNumber-1]
-obj.mod_allowed = obj.InputData['MOD_ALLOWED'][obj.TransactionNumber-1]
-obj.streamline_allowed = obj.InputData['STREAMLINE_ALLOWED'][obj.TransactionNumber-1]
-obj.investor_approval_required = obj.InputData['INVESTOR_APPROVAL_REQUIRED'][obj.TransactionNumber-1]
-obj.proprietary_mod_discount_approval_required = obj.InputData['PROPRIETARY_MOD_DISCOUNT_APPROVAL_REQUIRED'][obj.TransactionNumber-1]
-obj.non_interest_bearing_balloon_allowed = obj.InputData['NON_INTERST_BEARING_BALLOON_ALLOWED'][obj.TransactionNumber-1]
-obj.interest_rate_reduction_allowed = obj.InputData['INTEREST_RATE_REDUCTION_ALLOWED'][obj.TransactionNumber-1]
-obj.interest_bearing_balloon_allowed = obj.InputData['INTEREST_BEARING_BALLOON_ALLOWED'][obj.TransactionNumber-1]
-obj.maturity_extension_allowed = obj.InputData['MATURITY_EXTEMSION_ALLOWED'][obj.TransactionNumber-1]
-obj.deferment_allowed = obj.InputData['DEFERMENT_ALLOWED'][obj.TransactionNumber-1]
-obj.forgiveness_allowed = obj.InputData['FORGIVENESS_ALLOWED'][obj.TransactionNumber-1]
-obj.add_interim_month = obj.InputData['ADDINTERIMMONTH'][obj.TransactionNumber-1]
-obj.short_sale_allowed = obj.InputData['SHORT_SALE_ALLOWED'][obj.TransactionNumber-1]
-obj.short_sale_delegated = obj.InputData['SHORT_SALE_DELEGATED'][obj.TransactionNumber-1]
-obj.deed_in_lieu_allowed = obj.InputData['DEED_IN_LIEU_ALLOWED'][obj.TransactionNumber-1]
-obj.dil_delegated = obj.InputData['DIL_DELEGATED'][obj.TransactionNumber-1]
-obj.forbearance_allowed = obj.InputData['FORBEARANCE_ALLOWED'][obj.TransactionNumber-1]
-obj.message = obj.InputData['MESSAGE'][obj.TransactionNumber-1]
-obj.interest_rate_reduction_increments = obj.InputData['INTEREST_RATE_REDUCTION_INCREMENTS'][obj.TransactionNumber-1]
-obj.floor_rate = obj.InputData['FLOOR_RATE'][obj.TransactionNumber-1]
-obj.market_rate_type = obj.InputData['MARKET_RATE_TYPE'][obj.TransactionNumber-1]
-obj.number_of_mods_allowed = obj.InputData['NUMBER_OF_MODS_ALLOWED'][obj.TransactionNumber-1]
-obj.number_capitalizations_allowed = obj.InputData['NUMBER_CAPITALIZATIONS_ALLOWED'][obj.TransactionNumber-1]
-obj.max_term = obj.InputData['MAX_TERM'][obj.TransactionNumber-1]
-obj.minimum_credit_score = obj.InputData['MINIMUM_CREDIT_SCORE'][obj.TransactionNumber-1]
-obj.trial_duration = obj.InputData['TRIAL_DURATION'][obj.TransactionNumber-1]
-obj.forgiveness_upb_limit_percentage = obj.InputData['FORGIVENESS_UPB_LIMIT_PERCENTAGE'][obj.TransactionNumber-1]
-obj.forgiveness_lower_ltv_limit = obj.InputData['FORGIVENESS_LOWER_LTV_LIMIT'][obj.TransactionNumber-1]
-obj.sysdate = obj.InputData['SYSDATE'][obj.TransactionNumber-1]
-obj.maturity_date_restriction = obj.InputData['MATURITY_DATE_RESTRICTION'][obj.TransactionNumber-1]
-obj.min_days_delinquent = obj.InputData['MIN_DAYS_DELINQUENT'][obj.TransactionNumber-1]
-obj.imminent_default_max_days_delinquent = obj.InputData['IMMINENT_DEFAULT_MAX_DAYS_DELINQUENT'][obj.TransactionNumber-1]
-obj.min_pre_mod_dti = obj.InputData['MIN_PRE_MOD_DTI'][obj.TransactionNumber-1]
-obj.min_post_mod_dti = obj.InputData['MIN_POST_MOD_DTI'][obj.TransactionNumber-1]
-obj.maximum_post_mod_dti = obj.InputData['MAXIMUM_POST_MOD_DTI'][obj.TransactionNumber-1]
-obj.minimum_pre_mod_ltv = obj.InputData['MINIMUM_PRE_MOD_LTV'][obj.TransactionNumber-1]
-obj.forbearance_ltv_lower_limit = obj.InputData['FORBEARANCE_LTV_LOWER_LIMIT'][obj.TransactionNumber-1]
-obj.forbearance_upb_limit_percentage = obj.InputData['FORBEARANCE_UPB_LIMIT_PERCENTAGE'][obj.TransactionNumber-1]
-obj.payment_reduction_percentage = obj.InputData['PAYMENT_REDUCTION_PERCENTAGE'][obj.TransactionNumber-1]
-obj.decrements_for_forbearance_forgiveness = obj.InputData['DECREMENTS_FOR_FORBEARANCE_FORGIVENESS'][obj.TransactionNumber-1]
-obj.max_eval_day_of_the_month = obj.InputData['MAX_EVAL_DAY_OF_THE_MONTH'][obj.TransactionNumber-1]
-obj.streamline_min_days_delinquent = obj.InputData['STREAMLINE_MIN_DAYS_DELINQUENT'][obj.TransactionNumber-1]
-obj.maturity_date_extentions_by_month = obj.InputData['MATURITY_DATE_EXTENTIONS_BY_MONTH'][obj.TransactionNumber-1]
-obj.credit_bureau_expiration_days = obj.InputData['CREDIT_BUREAU_EXPIRATION_DAYS'][obj.TransactionNumber-1]
-obj.credit_bureau_min_days_delinquent = obj.InputData['CREDIT_BUREAU_MIN_DAYS_DELINQUENT'][obj.TransactionNumber-1]
-obj.mod_last_update = obj.InputData['MODLASTUPDATE'][obj.TransactionNumber-1]
-obj.fbl_last_update = obj.InputData['FBLASTUPDATE'][obj.TransactionNumber-1]
-obj.dil_last_update = obj.InputData['DILLASTUPDATE'][obj.TransactionNumber-1]
-obj.ssl_last_update = obj.InputData['SSLASTUPDATE'][obj.TransactionNumber-1]
+
+'INVESTOR_ID'
+'INVESTOR_DEAL_NAME'
+'INVESTOR_CATEGORY'
+'MOD_ALLOWED'
+'STREAMLINE_ALLOWED'
+'INVESTOR_APPROVAL_REQUIRED'
+'PROPRIETARY_MOD_DISCOUNT_APPROVAL_REQUIRED'
+'NON_INTERST_BEARING_BALLOON_ALLOWED'
+'INTEREST_RATE_REDUCTION_ALLOWED'
+'INTEREST_BEARING_BALLOON_ALLOWED'
+'MATURITY_EXTEMSION_ALLOWED'
+'DEFERMENT_ALLOWED'
+'FORGIVENESS_ALLOWED'
+'ADDINTERIMMONTH'
+'SHORT_SALE_ALLOWED'
+'SHORT_SALE_DELEGATED'
+'DEED_IN_LIEU_ALLOWED'
+'DIL_DELEGATED'
+'FORBEARANCE_ALLOWED'
+'MESSAGE'
+'INTEREST_RATE_REDUCTION_INCREMENTS'
+'FLOOR_RATE'
+'MARKET_RATE_TYPE'
+'NUMBER_OF_MODS_ALLOWED'
+'NUMBER_CAPITALIZATIONS_ALLOWED'
+'MAX_TERM'
+'MINIMUM_CREDIT_SCORE'
+'TRIAL_DURATION'
+'FORGIVENESS_UPB_LIMIT_PERCENTAGE'
+'FORGIVENESS_LOWER_LTV_LIMIT'
+'SYSDATE'
+'MATURITY_DATE_RESTRICTION'
+'MIN_DAYS_DELINQUENT'
+'IMMINENT_DEFAULT_MAX_DAYS_DELINQUENT'
+'MIN_PRE_MOD_DTI'
+'MIN_POST_MOD_DTI'
+'MAXIMUM_POST_MOD_DTI'
+'MINIMUM_PRE_MOD_LTV'
+'FORBEARANCE_LTV_LOWER_LIMIT'
+'FORBEARANCE_UPB_LIMIT_PERCENTAGE'
+'PAYMENT_REDUCTION_PERCENTAGE'
+'DECREMENTS_FOR_FORBEARANCE_FORGIVENESS'
+'MAX_EVAL_DAY_OF_THE_MONTH'
+'STREAMLINE_MIN_DAYS_DELINQUENT'
+'MATURITY_DATE_EXTENTIONS_BY_MONTH'
+'CREDIT_BUREAU_EXPIRATION_DAYS'
+'CREDIT_BUREAU_MIN_DAYS_DELINQUENT'
+'MODLASTUPDATE'
+'FBLASTUPDATE'
+'DILLASTUPDATE'
+'SSLASTUPDATE'
+'BOT Status'
+'BOT Run Date'
+'INVESTOR_DEAL_NAME_Updated ( Yes/No )'
+'INVESTOR_CATEGORY_Updated ( Yes/No )'
+'MOD_ALLOWED_Updated ( Yes/No )'
+'STREAMLINE_ALLOWED_Updated ( Yes/No )'
+'INVESTOR_APPROVAL_REQUIRED_Updated ( Yes/No )'
+'PROPRIETARY_MOD_DISCOUNT_APPROVAL_REQUIRED_Updated ( Yes/No )'
+'NON_INTERST_BEARING_BALLOON_ALLOWED_Updated ( Yes/No )'
+'INTEREST_RATE_REDUCTION_ALLOWED_Updated ( Yes/No )'
+'INTEREST_BEARING_BALLOON_ALLOWED_Updated ( Yes/No )'
+'MATURITY_EXTEMSION_ALLOWED_Updated ( Yes/No )'
+'DEFERMENT_ALLOWED_Updated ( Yes/No )'
+'FORGIVENESS_ALLOWED_Updated ( Yes/No )'
+'ADDINTERIMMONTH_Updated ( Yes/No )'
+'SHORT_SALE_ALLOWED_Updated ( Yes/No )'
+'SHORT_SALE_DELEGATED_Updated ( Yes/No )'
+'DEED_IN_LIEU_ALLOWED_Updated ( Yes/No )'
+'DIL_DELEGATED_Updated ( Yes/No )'
+'FORBEARANCE_ALLOWED_Updated ( Yes/No )'
+'MESSAGE_Updated ( Yes/No )'
+'INTEREST_RATE_REDUCTION_INCREMENTS_Updated ( Yes/No )'
+'FLOOR_RATE_Updated ( Yes/No )'
+'MARKET_RATE_TYPE_Updated ( Yes/No )'
+'NUMBER_OF_MODS_ALLOWED_Updated ( Yes/No )'
+'NUMBER_CAPITALIZATIONS_ALLOWED_Updated ( Yes/No )'
+'MAX_TERM_Updated ( Yes/No )'
+'MINIMUM_CREDIT_SCORE_Updated ( Yes/No )'
+'TRIAL_DURATION_Updated ( Yes/No )'
+'FORGIVENESS_UPB_LIMIT_PERCENTAGE_Updated ( Yes/No )'
+'FORGIVENESS_LOWER_LTV_LIMIT_Updated ( Yes/No )'
+'SYSDATE_Updated ( Yes/No )'
+'MATURITY_DATE_RESTRICTION_Updated ( Yes/No )'
+'MIN_DAYS_DELINQUENT_Updated ( Yes/No )'
+'IMMINENT_DEFAULT_MAX_DAYS_DELINQUENT_Updated ( Yes/No )'
+'MIN_PRE_MOD_DTI_Updated ( Yes/No )'
+'MIN_POST_MOD_DTI_Updated ( Yes/No )'
+'MAXIMUM_POST_MOD_DTI_Updated ( Yes/No )'
+'MINIMUM_PRE_MOD_LTV_Updated ( Yes/No )'
+'FORBEARANCE_LTV_LOWER_LIMIT_Updated ( Yes/No )'
+'FORBEARANCE_UPB_LIMIT_PERCENTAGE_Updated ( Yes/No )'
+'PAYMENT_REDUCTION_PERCENTAGE_Updated ( Yes/No )'
+'DECREMENTS_FOR_FORBEARANCE_FORGIVENESS_Updated ( Yes/No )'
+'MAX_EVAL_DAY_OF_THE_MONTH_Updated ( Yes/No )'
+'STREAMLINE_MIN_DAYS_DELINQUENT_Updated ( Yes/No )'
+'MATURITY_DATE_EXTENTIONS_BY_MONTH_Updated ( Yes/No )'
+'CREDIT_BUREAU_EXPIRATION_DAYS_Updated ( Yes/No )'
+'CREDIT_BUREAU_MIN_DAYS_DELINQUENT_Updated ( Yes/No )'
+
+obj.row[obj.headerlist.index('INVESTOR_ID')]=obj.investor_id
