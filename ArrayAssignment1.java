4f92f1353770fe4f086b3386c8e96faf048ddf12
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-05-30 02:29:13 +0530

    Update ArrayAssignment1.java

--- ArrayAssignment1.java
+++ ArrayAssignment1.java
@@ -1,36 +1,51 @@
-try:
-    in_mod_toggle = "YES"
-    bluergbcolor = (33,150,243)
-    mod_toggle = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div/div/main/div/div[2]/div/div[2]/div/div/div[3]/form/div[1]/div/div[2]/div/div[1]/div[2]/div/div/div/table/tbody/tr[1]/td[2]/div/label[2]/span')))
-    mod_toggle_color=mod_toggle.value_of_css_property('background-color')
-    mod_toggle_color=mod_toggle_color.replace('rgba','')
-    mod_toggle_color=eval(mod_toggle_color)
-    mod_toggle_hex='#{:02x}{:02x}{:02x}'.format(*mod_toggle_color)
-    print(mod_toggle_hex)
-    print("mod_toggle_hex is ",mod_toggle_hex)
-
-    #if (str(obj.hardship_resolved)).lower() == "yes":
-    if in_mod_toggle == "YES" :  
-        if  ''.join(map(str, bluergbcolor)) in mod_toggle_hex:
-            print("mod_toggle is already yes")
-            # obj.printlog.info(f"Hardship toggle already selected. input:{obj.hardship_resolved} 4.3")           
-        else:
-            mod_toggle.click()
-            print("Mod toggle selected")
-            # obj.printlog.info(f"Hardship toggle selected. input:{obj.hardship_resolved} 4.3")
-       
-    elif in_mod_toggle == "NO" :
-        if  ''.join(map(str, bluergbcolor)) in mod_toggle_hex:
-            mod_toggle.click()
-            print("Mod toggle unselected")
-            # obj.printlog.info(f"Hardship toggle unselected. input:{obj.hardship_resolved} 4.3")
-        else:
-            print("mod_toggle already toggle no")
-            # obj.printlog.info(f"Hardship toggle  already unselected. input:{obj.hardship_resolved} 4.3")
-    # obj.printlog.info("hardship toggle answered. 4.3")
-    # obj.driver.save_screenshot(obj.config['SS']+loanNumber+"FHA"+"Hardshiptoggle_4_3.png")
-except (TimeoutException, StaleElementReferenceException) as e:
-    traceback.print_exc()           
-    print(f"error:{e}")       
-except Exception as ex:
-    raise Exception(f"error:", str(ex)) 
+obj.investor_id = obj.InputData['INVESTOR_ID'][obj.TransactionNumber-1]
+obj.investor_deal_name = obj.InputData['INVESTOR_DEAL_NAME'][obj.TransactionNumber-1]
+obj.investor_category = obj.InputData['INVESTOR_CATEGORY'][obj.TransactionNumber-1]
+obj.mod_allowed = obj.InputData['MOD_ALLOWED'][obj.TransactionNumber-1]
+obj.streamline_allowed = obj.InputData['STREAMLINE_ALLOWED'][obj.TransactionNumber-1]
+obj.investor_approval_required = obj.InputData['INVESTOR_APPROVAL_REQUIRED'][obj.TransactionNumber-1]
+obj.proprietary_mod_discount_approval_required = obj.InputData['PROPRIETARY_MOD_DISCOUNT_APPROVAL_REQUIRED'][obj.TransactionNumber-1]
+obj.non_interest_bearing_balloon_allowed = obj.InputData['NON_INTERST_BEARING_BALLOON_ALLOWED'][obj.TransactionNumber-1]
+obj.interest_rate_reduction_allowed = obj.InputData['INTEREST_RATE_REDUCTION_ALLOWED'][obj.TransactionNumber-1]
+obj.interest_bearing_balloon_allowed = obj.InputData['INTEREST_BEARING_BALLOON_ALLOWED'][obj.TransactionNumber-1]
+obj.maturity_extension_allowed = obj.InputData['MATURITY_EXTEMSION_ALLOWED'][obj.TransactionNumber-1]
+obj.deferment_allowed = obj.InputData['DEFERMENT_ALLOWED'][obj.TransactionNumber-1]
+obj.forgiveness_allowed = obj.InputData['FORGIVENESS_ALLOWED'][obj.TransactionNumber-1]
+obj.add_interim_month = obj.InputData['ADDINTERIMMONTH'][obj.TransactionNumber-1]
+obj.short_sale_allowed = obj.InputData['SHORT_SALE_ALLOWED'][obj.TransactionNumber-1]
+obj.short_sale_delegated = obj.InputData['SHORT_SALE_DELEGATED'][obj.TransactionNumber-1]
+obj.deed_in_lieu_allowed = obj.InputData['DEED_IN_LIEU_ALLOWED'][obj.TransactionNumber-1]
+obj.dil_delegated = obj.InputData['DIL_DELEGATED'][obj.TransactionNumber-1]
+obj.forbearance_allowed = obj.InputData['FORBEARANCE_ALLOWED'][obj.TransactionNumber-1]
+obj.message = obj.InputData['MESSAGE'][obj.TransactionNumber-1]
+obj.interest_rate_reduction_increments = obj.InputData['INTEREST_RATE_REDUCTION_INCREMENTS'][obj.TransactionNumber-1]
+obj.floor_rate = obj.InputData['FLOOR_RATE'][obj.TransactionNumber-1]
+obj.market_rate_type = obj.InputData['MARKET_RATE_TYPE'][obj.TransactionNumber-1]
+obj.number_of_mods_allowed = obj.InputData['NUMBER_OF_MODS_ALLOWED'][obj.TransactionNumber-1]
+obj.number_capitalizations_allowed = obj.InputData['NUMBER_CAPITALIZATIONS_ALLOWED'][obj.TransactionNumber-1]
+obj.max_term = obj.InputData['MAX_TERM'][obj.TransactionNumber-1]
+obj.minimum_credit_score = obj.InputData['MINIMUM_CREDIT_SCORE'][obj.TransactionNumber-1]
+obj.trial_duration = obj.InputData['TRIAL_DURATION'][obj.TransactionNumber-1]
+obj.forgiveness_upb_limit_percentage = obj.InputData['FORGIVENESS_UPB_LIMIT_PERCENTAGE'][obj.TransactionNumber-1]
+obj.forgiveness_lower_ltv_limit = obj.InputData['FORGIVENESS_LOWER_LTV_LIMIT'][obj.TransactionNumber-1]
+obj.sysdate = obj.InputData['SYSDATE'][obj.TransactionNumber-1]
+obj.maturity_date_restriction = obj.InputData['MATURITY_DATE_RESTRICTION'][obj.TransactionNumber-1]
+obj.min_days_delinquent = obj.InputData['MIN_DAYS_DELINQUENT'][obj.TransactionNumber-1]
+obj.imminent_default_max_days_delinquent = obj.InputData['IMMINENT_DEFAULT_MAX_DAYS_DELINQUENT'][obj.TransactionNumber-1]
+obj.min_pre_mod_dti = obj.InputData['MIN_PRE_MOD_DTI'][obj.TransactionNumber-1]
+obj.min_post_mod_dti = obj.InputData['MIN_POST_MOD_DTI'][obj.TransactionNumber-1]
+obj.maximum_post_mod_dti = obj.InputData['MAXIMUM_POST_MOD_DTI'][obj.TransactionNumber-1]
+obj.minimum_pre_mod_ltv = obj.InputData['MINIMUM_PRE_MOD_LTV'][obj.TransactionNumber-1]
+obj.forbearance_ltv_lower_limit = obj.InputData['FORBEARANCE_LTV_LOWER_LIMIT'][obj.TransactionNumber-1]
+obj.forbearance_upb_limit_percentage = obj.InputData['FORBEARANCE_UPB_LIMIT_PERCENTAGE'][obj.TransactionNumber-1]
+obj.payment_reduction_percentage = obj.InputData['PAYMENT_REDUCTION_PERCENTAGE'][obj.TransactionNumber-1]
+obj.decrements_for_forbearance_forgiveness = obj.InputData['DECREMENTS_FOR_FORBEARANCE_FORGIVENESS'][obj.TransactionNumber-1]
+obj.max_eval_day_of_the_month = obj.InputData['MAX_EVAL_DAY_OF_THE_MONTH'][obj.TransactionNumber-1]
+obj.streamline_min_days_delinquent = obj.InputData['STREAMLINE_MIN_DAYS_DELINQUENT'][obj.TransactionNumber-1]
+obj.maturity_date_extentions_by_month = obj.InputData['MATURITY_DATE_EXTENTIONS_BY_MONTH'][obj.TransactionNumber-1]
+obj.credit_bureau_expiration_days = obj.InputData['CREDIT_BUREAU_EXPIRATION_DAYS'][obj.TransactionNumber-1]
+obj.credit_bureau_min_days_delinquent = obj.InputData['CREDIT_BUREAU_MIN_DAYS_DELINQUENT'][obj.TransactionNumber-1]
+obj.mod_last_update = obj.InputData['MODLASTUPDATE'][obj.TransactionNumber-1]
+obj.fbl_last_update = obj.InputData['FBLASTUPDATE'][obj.TransactionNumber-1]
+obj.dil_last_update = obj.InputData['DILLASTUPDATE'][obj.TransactionNumber-1]
+obj.ssl_last_update = obj.InputData['SSLASTUPDATE'][obj.TransactionNumber-1]
