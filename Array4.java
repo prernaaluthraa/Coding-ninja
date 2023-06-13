bce4c064aee51d3b7c2b4c88b7a03215483ab5f8
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-06-13 15:11:13 +0530

    Update Array4.java

--- Array4.java
+++ Array4.java
@@ -1,29 +1,41 @@
 
-def element_exists(driver, selector, exists_action=None, Non_exists_action=None):
-    
-    elements = driver.find_elements(By.XPATH, '/html/body/div/div/main/div/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div[1]/div/div[3]/div[2]/div/div/div/div[6]/div/button')
-    if elements:
-        element = elements[0]   
-        if exists_action:
-            exists_action(element)
-        return True
-    else:
-        if Non_exists_action:
-            Non_exists_action()
-        return False    
+# Fetch all the results
+results = cursor.fetchall()
+print("Number of rows in results:", len(results))
+print("Number of columns in results:", len(results[0]))
+print("Number of column names:", column_names)
 
-def on_edit_exists(element):
-    element.click()
-    print("edit clicked")
+# Create a DataFrame from the results and column names
+df = pd.DataFrame.from_records(results, columns=column_names)
 
-def on_edit_not_exists():
-    print("edit doesn't exist")
-    driver.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div/div[2]/div/div/div[2]/div/div[1]/button').click()
-    print("new record clicked")
+print("DataFrame shape:", df.shape)
 
+# Save DataFrame to CSV file without index column
+InputFile = 'InputFile_' + datetime.now().strftime('%m-%d-%Y') + '.csv'
+df.to_csv('D:/RPA/Prerna/Investor Rules/' + InputFile, index=False)
 
+# Save DataFrame to Excel file with styled column headers
+ExcelFile = 'InputFile_' + datetime.now().strftime('%m-%d-%Y') + '.xlsx'
+data1 = df[['INVESTOR_ID', 'INVESTOR_DEAL_NAME', 'INVESTOR_CATEGORY', 'MOD_ALLOWED']]
+data1.to_excel('D:/RPA/Prerna/Investor Rules/' + ExcelFile, index=False)
 
-element_exists(driver,"/html/body/div/div/main/div/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div/div/div[1]/div/div[3]/div[2]/div/div/div/div[6]/div/button",exists_action=on_edit_exists, Non_exists_action=on_edit_not_exists)
-   
+# Open the Excel file with openpyxl
+workbook = openpyxl.load_workbook('D:/RPA/Prerna/Investor Rules/' + ExcelFile)
+worksheet = workbook.active
+
+# Define the fill color for the column headers
+header_fill = PatternFill(start_color="FFFF00", end_color="FFFF00", fill_type="solid")
+
+# Apply the fill color to the column headers
+for column in range(1, data1.shape[1] + 1):
+    cell = worksheet.cell(row=1, column=column)
+    cell.fill = header_fill
+
+# Save the modified Excel file
+workbook.save('D:/RPA/Prerna/Investor Rules/' + ExcelFile)
+
+# Close the cursor and connection
+cursor.close()
+cnxn.close()
 
 
