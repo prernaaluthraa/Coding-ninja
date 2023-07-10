cfaf9084f9b952f88b0b5332cb227f4906ffecf0
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-07-10 09:10:31 -0400

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -1,15 +1,36 @@
     try:
-        time.sleep(2)
-        #in_interestrate = '0.0125'
-        interestrate_element = driver.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div/div[2]/div/div/div[3]/form/div[1]/div/div[2]/div/div[2]/div[2]/div[1]/div/div/div/div/table/tbody/tr[1]/td[2]/div/div/div/span/input')
-        print("interestrate field to be updated",obj.interest_rate_reduction_increments)
-        obj.printlog.info(f"interestrate field to be updated",obj.interest_rate_reduction_increments)
+        # obj.mod_allowed = "YES"
+        mod_toggle = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div/div/main/div/div[2]/div/div[2]/div/div/div[3]/form/div[1]/div/div[2]/div/div[1]/div[2]/div/div/div/table/tbody/tr[1]/td[2]/div/div/div/div/label[2]/span')))
+        mod_toggle_color=mod_toggle.value_of_css_property('background-color')
+        mod_toggle_color=mod_toggle_color.replace('rgba','')
+        mod_toggle_color=eval(mod_toggle_color)
+        mod_toggle_hex='#{:02x}{:02x}{:02x}'.format(*mod_toggle_color)
+        print(mod_toggle_hex)
+        print("mod_toggle_hex is ",mod_toggle_hex)
+        obj.printlog.info(f"mod_toggle_hex is ",mod_toggle_hex)
 
-        interestrate_element.send_keys(obj.interest_rate_reduction_increments)
-        print("interestrate_element updated.", obj.interest_rate_reduction_increments)    
-        obj.printlog.info(f"interestrate_element updated." , obj.interest_rate_reduction_increments)
+        if obj.mod_allowed == "YES" :  
+            if '#2196f3' in mod_toggle_hex:    
+                print("Mod toggle already selected")
+                obj.printlog.info(f"Mod toggle already selected. input:{obj.mod_allowed} 1")           
+            else:
+                mod_toggle.click()
+                obj.mod_allowed_updated = "Yes"
+                print("Mod toggle selected")
+                obj.printlog.info(f"Mod toggle selected. input:{obj.mod_allowed} 1")   
+        else:
+            if '#2196f3' in mod_toggle_hex:
+                mod_toggle.click()
+                obj.mod_allowed_updated = "Yes"
+                print("Mod toggle unselected")
+                obj.printlog.info(f"mod toggle unselected. input:{obj.mod_allowed} 1")
+            else:
+                print("mod_toggle already toggle no")
+                obj.printlog.info(f"Mod toggle  already unselected. input:{obj.mod_allowed} 1")
+        obj.printlog.info("Mod toggle answered. 4.3")
+        obj.driver.save_screenshot(obj.config['SS']+investorid+"Modtoggle_1.png")
     except (TimeoutException, StaleElementReferenceException) as e:
-        traceback.print_exc()
-        print(f"error:{e}")     
+        traceback.print_exc()           
+        print(f"error:{e}")       
     except Exception as ex:
-        raise Exception(f"error:", str(ex))
+        raise Exception(f"error:", str(ex))    
