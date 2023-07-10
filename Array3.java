bf1ee80586b7ae53050e1ec702c879f762aaff4d
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-07-10 15:34:59 -0400

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -1,36 +1,21 @@
     try:
-        # obj.mod_allowed = "YES"
-        mod_toggle = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div/div/main/div/div[2]/div/div[2]/div/div/div[3]/form/div[1]/div/div[2]/div/div[1]/div[2]/div/div/div/table/tbody/tr[1]/td[2]/div/div/div/div/label[2]/span')))
-        mod_toggle_color=mod_toggle.value_of_css_property('background-color')
-        mod_toggle_color=mod_toggle_color.replace('rgba','')
-        mod_toggle_color=eval(mod_toggle_color)
-        mod_toggle_hex='#{:02x}{:02x}{:02x}'.format(*mod_toggle_color)
-        print(mod_toggle_hex)
-        print("mod_toggle_hex is ",mod_toggle_hex)
-        obj.printlog.info(f"mod_toggle_hex is ",mod_toggle_hex)
-
-        if obj.mod_allowed == "YES" :  
-            if '#2196f3' in mod_toggle_hex:    
-                print("Mod toggle already selected")
-                obj.printlog.info(f"Mod toggle already selected. input:{obj.mod_allowed} 1")           
-            else:
-                mod_toggle.click()
-                obj.mod_allowed_updated = "Yes"
-                print("Mod toggle selected")
-                obj.printlog.info(f"Mod toggle selected. input:{obj.mod_allowed} 1")   
+        time.sleep(2)
+        # obj.message = 'yes ONLY STANDARD FREDDIE MODS ALLOWED'
+        message_element = driver.find_element(By.XPATH, '/html/body/div/div/main/div/div[2]/div/div[2]/div/div/div[3]/form/div[1]/div/div[3]/div/div/div[2]/div/div[1]/div/textarea')
+        message_text = message_element.text
+        print("Text from the message field",message_text)
+        obj.printlog.info(f"Text from the message field",message_text)
+        if message_text == obj.message:
+            print("message in the bkfs field matched with the output")
+            obj.printlog.info(f"message in the bkfs field matched with the output")
         else:
-            if '#2196f3' in mod_toggle_hex:
-                mod_toggle.click()
-                obj.mod_allowed_updated = "Yes"
-                print("Mod toggle unselected")
-                obj.printlog.info(f"mod toggle unselected. input:{obj.mod_allowed} 1")
-            else:
-                print("mod_toggle already toggle no")
-                obj.printlog.info(f"Mod toggle  already unselected. input:{obj.mod_allowed} 1")
-        obj.printlog.info("Mod toggle answered. 4.3")
-        obj.driver.save_screenshot(obj.config['SS']+investorid+"Modtoggle_1.png")
+            message_element.send_keys(Keys.CONTROL + "a")
+            message_element.send_keys(Keys.BACKSPACE)
+            message_element.send_keys(obj.message)
+            print("message replaced.")
+            obj.printlog.info(f"message replaced ",obj.message)
     except (TimeoutException, StaleElementReferenceException) as e:
         traceback.print_exc()           
         print(f"error:{e}")       
     except Exception as ex:
-        raise Exception(f"error:", str(ex))    
+        raise Exception(f"error:", str(ex)) 
