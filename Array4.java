f5014140ea97fab1efc93594f522b09b88043dc6
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-07-17 17:20:03 -0400

    Update Array4.java

--- Array4.java
+++ Array4.java
@@ -1,106 +1,95 @@
-import os
-import time
-import csv
-import shutil
-import uuid
+import pyodbc
+import pandas as pd
+import openpyxl
+from datetime import date, datetime, timedelta
+import openpyxl.styles as styles
 
-def cleanup(obj):
-    try:
-        rpa_path = "/application/RPA"
-        folders_to_check = ["LOGS", "SCREENSHOTS", "ARCHIVE", "OUT"]
-        three_days_ago = time.time() - (90 * 24 * 60 * 60)
-        seven_days_ago = time.time() - (7 * 24 * 60 * 60)
-        log_count = 0
-        screenshot_count = 0
-        others_count = 0
-        size = 0
 
-        output_filename = "/application/RPA/COMMON/CleanupFiles/LOGS/output_" + time.strftime("%Y%m%d-%H%M%S") + ".csv"
-        with open(output_filename, 'w', newline='') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            csvwriter.writerow(["Action", "File Path", "Modification Date"])
+def last_working_date():
+    today = date.today()
+    weekday = today.weekday()
 
-            # Get disk storage before running the script
-            total_before = os.statvfs("/application/RPA").f_frsize * os.statvfs("/application/RPA").f_blocks
-            free_before = os.statvfs("/application/RPA").f_frsize * os.statvfs("/application/RPA").f_bfree
+    if weekday == 0:
+        last_working_day = today - timedelta(days=3)
+    else:
+        last_working_day = today - timedelta(days=1)
 
-            # Convert sizes to human-readable format
-            total_before_str = f"{total_before / (1024 ** 3):.2f} GB"
-            free_before_str = f"{free_before / (1024 ** 3):.2f} GB"
+    formatted_date = last_working_day.strftime("%m/%d/%Y")
+    print("Last working day:", formatted_date)
+    return formatted_date
 
-            # Print disk storage information before running the script
-            csvwriter.writerow(["Disk Storage Before Script Execution"])
-            csvwriter.writerow(["Total", total_before_str])
-            csvwriter.writerow(["Free", free_before_str])
-            csvwriter.writerow([])  # Empty row for separation
 
-            # Create a temporary folder with a unique identifier appended to the name
-            temp_folder_name = "temp_" + time.strftime("%Y%m%d-%H%M%S") + "_" + str(uuid.uuid4())
-            temp_folder_path = os.path.join("/application/RPA/COMMON/CleanupFiles/DeletedFiles", temp_folder_name)
-            os.makedirs(temp_folder_path)
+formatted_date = last_working_date()
 
-            # Check files in the "deleted_files" folder
-            for folder in os.listdir(temp_folder_path):
-                folder_path = os.path.join(temp_folder_path, folder)
-                if os.path.isdir(folder_path) and os.path.getmtime(folder_path) < seven_days_ago:
-                    # Delete files in the folder
-                    for file in os.listdir(folder_path):
-                        file_path = os.path.join(folder_path, file)
-                        if os.path.isfile(file_path):
-                            csvwriter.writerow(["Deleting file", file_path, ""])
-                            os.remove(file_path)
-                    # Delete the folder
-                    shutil.rmtree(folder_path)
+def read_query(file_path):
+    with open(file_path, 'r') as file:
+        query = file.read()
+    return query
 
-            # Continue with the existing code to move files older than 90 days
-            for root, dirs, files in os.walk(rpa_path):
-                for folder_name in folders_to_check:
-                    if folder_name in dirs:
-                        folder_path = os.path.join(root, folder_name)
-                        csvwriter.writerow(["Checking", folder_path, ""])
+query_file_path = '/application/RPA/LRPU/PLSInvestorRulesAutomation/REQUIRED/Query.txt'
+# filename = 'data.xlsx'   
+query = read_query(query_file_path)
+query = query.replace('date',formatted_date)
+print(query)
 
-                        # Move the files to the temporary folder instead of deleting
-                        for filename in os.listdir(folder_path):
-                            file_path = os.path.join(folder_path, filename)
-                            if os.path.isfile(file_path) and os.path.getmtime(file_path) < three_days_ago:
-                                mod_time = os.path.getmtime(file_path)
-                                mod_date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(mod_time))
-                                
-                                # Generate a unique identifier for the destination filename
-                                unique_filename = str(uuid.uuid4()) + "_" + filename
-                                move_path = os.path.join(temp_folder_path, unique_filename)
-                                
-                                csvwriter.writerow(["File path", file_path, mod_date])
-                                
-                                # Check if the destination file already exists
-                                while os.path.exists(move_path):
-                                    # Append a unique identifier to the filename
-                                    unique_filename = str(uuid.uuid4()) + "_" + filename
-                                    move_path = os.path.join(temp_folder_path, unique_filename)
-                                
-                                # Move the file to the temporary folder with the unique filename
-                                shutil.move(file_path, move_path)
+drivers = [item for item in pyodbc.drivers()]
+print("Establishing connection")
+print("############### DRIVERS",drivers)
+driver = drivers[0]
+server = '10.72.8.53,50011'
+database = 'InvestorRulesMSP'
+uid = 'svcs_rpa_prod_bot1'
+pwd = 'OcwenBOAT@123'
+SSO = 'Trusted_Connection=yes'
+timeout = '30'
+con_string = f'driver={driver};server={server};database={database};uid={uid};pwd={pwd};'
+cnxn = pyodbc.connect(con_string)
+print("connection established")
+cursor = cnxn.cursor()
+print("connection established, yes")
 
-            # Get disk storage after running the script
-            total_after = os.statvfs("/application/RPA").f_frsize * os.statvfs("/application/RPA").f_blocks
-            free_after = os.statvfs("/application/RPA").f_frsize * os.statvfs("/application/RPA").f_bfree
+# Execute the query
+cursor.execute(query)
 
-            # Calculate used space
-            used_before = total_before - free_before
-            used_after = total_after - free_after
+# Get the column names
 
-            # Convert sizes to human-readable format
-            total_after_str = f"{total_after / (1024 ** 3):.2f} GB"
-            used_after_str = f"{used_after / (1024 ** 3):.2f} GB"
-            free_after_str = f"{free_after / (1024 ** 3):.2f} GB"
+column_names = [description[0] for description in cursor.description]
 
-            # Print disk storage information after running the script
-            csvwriter.writerow([])  # Empty row for separation
-            csvwriter.writerow(["Disk Storage After Script Execution"])
-            csvwriter.writerow(["Total", total_after_str])
-            csvwriter.writerow(["Used", used_after_str])
-            csvwriter.writerow(["Free", free_after_str])
+# Fetch all the results
+results = cursor.fetchall()
+print("Number od rows in results", len(results))
+print("Number od columns in results",len(results[0]))
+print("Number of column names",column_names)
+# Create a DataFrame from the results and column names
+df = pd.DataFrame.from_records(results, columns = column_names)
+print("dataframeshape",df.shape)
+# df = pd.DataFrame(results, columns = ['INVESTOR_ID','INVESTOR_DEAL_NAME','INVESTOR_CATEGORY','MOD_ALLOWED','STREAMLINE_ALLOWED','INVESTOR_APPROVAL_REQUIRED','PROPRIETARY_MOD_DISCOUNT_APPROVAL_REQUIRED','NON_INTERST_BEARING_BALLOON_ALLOWED','INTEREST_RATE_REDUCTION_ALLOWED','INTEREST_BEARING_BALLOON_ALLOWED','MATURITY_EXTEMSION_ALLOWED','DEFERMENT_ALLOWED','FORGIVENESS_ALLOWED','ADDINTERIMMONTH','SHORT_SALE_ALLOWED','SHORT_SALE_DELEGATED','DEED_IN_LIEU_ALLOWED','DIL_DELEGATED','FORBEARANCE_ALLOWED','MESSAGE','INTEREST_RATE_REDUCTION_INCREMENTS','FLOOR_RATE','MARKET_RATE_TYPE','NUMBER_OF_MODS_ALLOWED','NUMBER_CAPITALIZATIONS_ALLOWED','MAX_TERM','MINIMUM_CREDIT_SCORE','TRIAL_DURATION','FORGIVENESS_UPB_LIMIT_PERCENTAGE','FORGIVENESS_LOWER_LTV_LIMIT','SYSDATE','MATURITY_DATE_RESTRICTION','MIN_DAYS_DELINQUENT','IMMINENT_DEFAULT_MAX_DAYS_DELINQUENT','MIN_PRE_MOD_DTI','MIN_POST_MOD_DTI','MAXIMUM_POST_MOD_DTI','MINIMUM_PRE_MOD_LTV','FORBEARANCE_LTV_LOWER_LIMIT','FORBEARANCE_UPB_LIMIT_PERCENTAGE','PAYMENT_REDUCTION_PERCENTAGE','DECREMENTS_FOR_FORBEARANCE_FORGIVENESS','MAX_EVAL_DAY_OF_THE_MONTH','STREAMLINE_MIN_DAYS_DELINQUENT','MATURITY_DATE_EXTENTIONS_BY_MONTH','CREDIT_BUREAU_EXPIRATION_DAYS','CREDIT_BUREAU_MIN_DAYS_DELINQUENT','MODLASTUPDATE','FBLASTUPDATE','DILLASTUPDATE','SSLASTUPDATE'])
 
-    except Exception as ex:
-        obj.SystemException = "Cleanup Script failed. Please look into it."
-        raise Exception("Error:", str(ex))
+# InputFile='InputFile_'+datetime.now().strftime('%m-%d-%Y')+'.csv'
+# df.to_csv('D:/RPA/Prerna/Investor Rules/'+InputFile, index=False)
+InputFile = 'BKFS PLS Investor Rules Input Data_' + datetime.now().strftime('%m.%d.%Y') + '.xlsx'
+df.to_excel('/application/RPA/LRPU/PLSInvestorRulesAutomation/IN/' + InputFile, index=False)
+
+# Load the Excel file using openpyxl
+wb = openpyxl.load_workbook('/application/RPA/LRPU/PLSInvestorRulesAutomation/IN/' + InputFile)
+
+# Select the active sheet
+sheet = wb.active
+
+# Apply formatting to column headers
+header_font = styles.Font(color="000000", bold=True)
+header_fill = styles.PatternFill(fill_type="solid", fgColor="FFCC99")
+
+for col_idx, column_name in enumerate(column_names, start=1):
+    cell = sheet.cell(row=1, column=col_idx)
+    cell.font = header_font
+    cell.fill = header_fill
+
+# Save the modified Excel file
+wb.save('/application/RPA/LRPU/PLSInvestorRulesAutomation/IN/' + InputFile)
+
+# Close the cursor and connection
+cursor.close()
+cnxn.close()
+
+print("Results saved to input file")
