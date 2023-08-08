22dcde667d34dac4b87819f442aa2a4ff2ac2f28
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-08-08 15:04:15 +0530

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -1,108 +1,49 @@
-import sys
-from datetime import datetime
-sys.path.insert(0, '/application/RPA/COMMON/STANDARD_OCR/SRC/')
-import StandardFormExtract_page as sd
-import os
-import pathlib
-import bs4
-from pdf2image import convert_from_path
-from pytesseract import pytesseract
-from PyPDF2 import PdfWriter, PdfReader
-
-print("Starting the process at",datetime.now())
-#Full file path, document type (urla, cd etc). File format (PDF or TIFF)
-# ********************* Pre- Proceessing - Page Removing code **************************** #
-
-# def preProcessing(pdfFileName):
-#     PDF = pdfFileName
-#     print(PDF)
-#     hocrfilename=pathlib.Path(PDF).stem
-#     print(hocrfilename)
-#     Tiffilename= hocrfilename + ".tiff"
-#     print(Tiffilename)
-
-
-
-
-#     # Tiffilename="Appraisal (2-4 Units).tiff"
-#     # PDF="/application/RPA/COMMON/STANDARD_OCR/TEMP_DEV/Gunveer/Appraisal (2-4 Units).pdf"
-    
-#     pages = convert_from_path(PDF,first_page=1,last_page=7)
-
-#     #Get first 7 pages
-#     try:
-#         pages[0].save(Tiffilename, format="tiff", append_images=[pages[1],pages[2],pages[3],pages[4],pages[5],pages[6]], save_all=True, duration=500, loop=0)
-#     except:
-#         pages[0].save(Tiffilename)
-#     custom_oem_psm_config = r'-c tessedit_do_invert=0 --oem 1 --psm 6'
-
-#     pytesseract.run_tesseract(Tiffilename, hocrfilename, lang=None, extension='hocr',config=custom_oem_psm_config)
-#     xml_input = open(hocrfilename+".hocr","r",encoding="utf-8")
-
-#     ######## Parsing xml data usign Beautiful soup and passing only OCR lines for extraction
-#     soup = bs4.BeautifulSoup(xml_input,'lxml')
-#     ocr_lines = soup.findAll("span", {"class": "ocr_line"})
-#     xml_input.close()
-#     propAppripage=1
-#     for line in ocr_lines:
-#         line_text= line.text.replace("\n"," ").strip().replace('  ',' ')
-#         if 'Small Residential Income Property Appraisal Report' in line_text:
-#             propAppripage = str(line.parent.parent.parent['id']).split("_")[1]
-#             print(propAppripage)
-#             break
-
-#     os.remove(hocrfilename+".hocr")
-#     os.remove(hocrfilename+".tiff")
-#     infile = PdfReader(PDF)
-#     output = PdfWriter()
-#     for i in range(len(infile.pages)):
-#         if i<int(propAppripage) - 1:
-#             pass
-#         else:
-#             p=infile.pages[i]
-#             output.add_page(p)
-#     # with open(hocrfilename+'_updated.pdf', 'wb') as f:
-#     #     output.write(f)
-
-#     newpdf = '/application/RPA/COMMON/STANDARD_OCR/TEMP_DEV/Gunveer/'+ hocrfilename + '_updated.pdf'
-#     print('outputfile is '+newpdf)
-
-#     with open(newpdf, 'wb') as f:
-#         output.write(f)
-
-#     return newpdf
-    
-    
-
-
-# # ************************ Farmework Func code to extract values with bounding box *********************************#
-# # New file name should be provide as 1st argument in Func
-# newpdf = preProcessing('/application/RPA/COMMON/STANDARD_OCR/TEMP_DEV/Gunveer/Appraisal (2-4 Units).pdf')
-form_dict= sd.form_extract_initiate('/application/RPA/COMMON/STANDARD_OCR/TEMP_DEV/Gunveer/Appraisal (2-4 Units).pdf', "TEMP_PROP_APPRAISAL", "PDF")
-print(form_dict)
-print("Execution Complete at",datetime.now())
-
-# ********************* Post - Processing (RegEx or String Manuplation)****************************
-form_dict['Company Address'][0] = form_dict['Company Address'][0].replace("Company Address","").strip()
-print(form_dict)
-
-
-form_dict['APPRAISED VALUE'][0] = form_dict['APPRAISED VALUE'][0].replace("APPRAISED VALUE","").strip()
-print(form_dict)
-
-
-form_dict['Appraisal_TYPE'][0] = form_dict['Appraisal_TYPE'][1].replace("Appraisal_TYPE","").strip()
-print(form_dict)
-
-
-
-form_dict['Appraisal_TYPE'][0] = form_dict['Appraisal_TYPE'][0].replace("Appraisal_TYPE","").strip()
-appraisal_type_words = form_dict['Appraisal_TYPE'][0].split()  # Split the string into words
-if len(appraisal_type_words) >= 2:
-    try:
-        form_dict['Appraisal_TYPE'][0] = int(appraisal_type_words[1])  # Convert the 2nd word to an integer
-    except ValueError:
-        form_dict['Appraisal_TYPE'][0] = None  # Handle the case where conversion fails
-else:
-    form_dict['Appraisal_TYPE'][0] = None  # Set to None if there are less than 2 words
-print(form_dict)
+obj.investor_id = obj.InputData['INVESTOR_IDNumber'][obj.TransactionNumber-1]
+obj.investor_category = obj.InputData['INVESTOR_CATEGORY'][obj.TransactionNumber-1]
+obj.mod_allowed = obj.InputData['MOD_ALLOWED'][obj.TransactionNumber-1]
+obj.streamline_allowed = obj.InputData['STREAMLINE_ALLOWED'][obj.TransactionNumber-1]
+obj.investor_approval_required = obj.InputData['INVESTOR_APPROVAL_REQUIRED'][obj.TransactionNumber-1]
+obj.proprietary_mod_discount_approval_required = obj.InputData['PROPRIETARY_MOD_DISCOUNT_APPROVAL_REQUIRED'][obj.TransactionNumber-1]
+obj.non_interest_bearing_balloon_allowed = obj.InputData['NON_INTERST_BEARING_BALLOON_ALLOWED'][obj.TransactionNumber-1]
+obj.interest_rate_reduction_allowed = obj.InputData['INTEREST_RATE_REDUCTION_ALLOWED'][obj.TransactionNumber-1]
+obj.interest_bearing_balloon_allowed = obj.InputData['INTEREST_BEARING_BALLOON_ALLOWED'][obj.TransactionNumber-1]
+obj.maturity_extension_allowed = obj.InputData['MATURITY_EXTENSION_ALLOWED'][obj.TransactionNumber-1]
+obj.deferment_allowed = obj.InputData['DEFERMENT_ALLOWED'][obj.TransactionNumber-1]
+obj.forgiveness_allowed = obj.InputData['FORGIVENESS_ALLOWED'][obj.TransactionNumber-1]
+obj.add_interim_month = obj.InputData['AddInterimMonth'][obj.TransactionNumber-1]
+obj.short_sale_allowed = obj.InputData['SHORT_SALE_ALLOWED'][obj.TransactionNumber-1]
+obj.short_sale_delegated = obj.InputData['SHORT_SALE_DELEGATED'][obj.TransactionNumber-1]
+obj.dil_allowed = obj.InputData['DIL_ALLOWED'][obj.TransactionNumber-1]
+obj.dil_delegated = obj.InputData['DIL_DELEGATED'][obj.TransactionNumber-1]
+obj.forbearance_plan_allowed = obj.InputData['FORBEARANCE_PLAN_ALLOWED'][obj.TransactionNumber-1]
+obj.message = obj.InputData['Message'][obj.TransactionNumber-1]
+obj.interest_rate_reduction_increments = obj.InputData['INTEREST_RATE_REDUCTION_INCREMENTS'][obj.TransactionNumber-1]
+obj.floor_rate = obj.InputData['FLOOR_RATE'][obj.TransactionNumber-1]
+obj.market_rate_type = obj.InputData['MARKET_RATE_TYPE'][obj.TransactionNumber-1]
+obj.number_of_mods_allowed = obj.InputData['NUMBER_OF_MODS_ALLOWED'][obj.TransactionNumber-1]
+obj.number_capitalizations_allowed = obj.InputData['NUMBER_CAPITALIZATIONS_ALLOWED'][obj.TransactionNumber-1]
+obj.max_term = obj.InputData['MAX_TERM'][obj.TransactionNumber-1]
+obj.minimum_credit_score = obj.InputData['MINIMUM_CREDIT_SCORE'][obj.TransactionNumber-1]
+obj.trial_duration = obj.InputData['TRIAL_DURATION'][obj.TransactionNumber-1]
+obj.forgiveness_upb_limit_percentage = obj.InputData['FORGIVENESS_UPB_LIMIT_PERCENTAGE'][obj.TransactionNumber-1]
+obj.forgiveness_lower_ltv_limit = obj.InputData['FORGIVENESS_LOWER_LTV_LIMIT'][obj.TransactionNumber-1]
+obj.maturity_restriction_date = obj.InputData['MATURITY_RESTRICTION_DATE'][obj.TransactionNumber-1]
+obj.min_days_delinquent = obj.InputData['MIN_DAYS_DELINQUENT'][obj.TransactionNumber-1]
+obj.imminent_default_max_days_delinquent = obj.InputData['IMMINENT_DEFAULT_MAX_DAYS_DELINQUENT'][obj.TransactionNumber-1]
+obj.min_pre_mod_dti = obj.InputData['MIN_PRE_MOD_DTI'][obj.TransactionNumber-1]
+obj.min_post_mod_dti = obj.InputData['MIN_POST_MOD_DTI'][obj.TransactionNumber-1]
+obj.maximum_post_mod_dti = obj.InputData['MAXIMUM_POST_MOD_DTI'][obj.TransactionNumber-1]
+obj.min_pre_mod_ltv = obj.InputData['MIN_PRE_MOD_LTV'][obj.TransactionNumber-1]
+obj.forbearance_ltv_lower_limit = obj.InputData['FORBEARANCE_LTV_LOWER_LIMIT'][obj.TransactionNumber-1]
+obj.forbearance_upb_limit_percentage = obj.InputData['FORBEARANCE_UPB_LIMIT_PERCENTAGE'][obj.TransactionNumber-1]
+obj.payment_reduction_percentage = obj.InputData['PAYMENT_REDUCTION_PERCENTAGE'][obj.TransactionNumber-1]
+obj.decrements_for_forbearance_forgiveness = obj.InputData['DECREMENTS_FOR_FORBEARANCE_FORGIVENESS'][obj.TransactionNumber-1]
+obj.max_eval_day_of_the_month = obj.InputData['MAX_EVAL_DAY_OF_THE_MONTH'][obj.TransactionNumber-1]
+obj.streamline_min_days_delinquent = obj.InputData['STREAMLINE_MIN_DAYS_DELINQUENT'][obj.TransactionNumber-1]
+obj.maturity_date_extensions_by_month = obj.InputData['MATURITY_DATE_EXTENTIONS_BY_MONTH'][obj.TransactionNumber-1]
+obj.credit_bureau_expiration_days = obj.InputData['CREDIT_BUREAU_EXPIRATION_DAYS'][obj.TransactionNumber-1]
+obj.credit_bureau_min_days_delinquent = obj.InputData['CREDIT_BUREAU_MIN_DAYS_DELINQUENT'][obj.TransactionNumber-1]
+obj.mod_last_update = obj.InputData['MODLASTUPDATE'][obj.TransactionNumber-1]
+obj.fbl_last_update = obj.InputData['FBLASTUPDATE'][obj.TransactionNumber-1]
+obj.dil_last_update = obj.InputData['DILLASTUPDATE'][obj.TransactionNumber-1]
+obj.ssl_last_update = obj.InputData['SSLASTUPDATE'][obj.TransactionNumber-1]
