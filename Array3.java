beff4286b80ef515a0244c0ac3632719ba43c6c8
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-08-09 16:49:16 -0400

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -1,32 +1,19 @@
-element_exists(driver,"/html/body/div/div/div[2]/main/div/div/div[2]/div/div/div[2]/div/div[2]/div/div/div[1]/div[2]/div[3]/div[1]/div/div[2]/div/div/div/div[6]/div/span/button",exists_action=on_edit_exists, Non_exists_action=on_edit_not_exists, obj)
+file inputsql
+def readDataInputSQL(obj):
+    query_file_path = obj.config['REQUIRED']+'Query.txt'
+    # filename = 'data.xlsx'
+    today = date.today()
+    weekday = today.weekday()
 
-
-
-def element_exists(driver, selector, exists_action=None, Non_exists_action=None,obj):
-    
-    elements = driver.find_elements(By.XPATH, '/html/body/div/div/div[2]/main/div/div/div[2]/div/div/div[2]/div/div[2]/div/div/div[1]/div[2]/div[3]/div[1]/div/div[2]/div/div/div/div[6]/div/span/button')
-    if elements:
-        element = elements[0]   
-        if exists_action:
-            exists_action(element)
-        return True
+    if weekday == 0:
+        last_working_day = today - timedelta(days=3)
     else:
-        if Non_exists_action:
-            Non_exists_action()
-        return False    
+        last_working_day = today - timedelta(days=1)
 
-def on_edit_exists(obj,element):
-    element.click()
-    print("edit clicked")
-    # toggle.validatequest(obj)
-    # vdfield.validateQuestField(obj)
-    # vdfieldlast.validateQuestFieldLast(obj)
+    formatted_date = last_working_day.strftime("%m/%d/%Y")
+    print("Last working day:", formatted_date)
+    query = obj.read_query(query_file_path)
+    query = query.replace('date', formatted_date)
+    print(query)
 
-    obj.printlog.info("Edit exists and clicked")
-
-def on_edit_not_exists(obj):
-    print("edit doesn't exist")
-    obj.printlog.info("New Record as edit doesn't exist")
-    driver.find_element(By.XPATH, '/html/body/div/div/div[2]/main/div/div/div[2]/div/div/div[2]/div/div[1]/button').click()
-    print("new record clicked")
-    obj.printlog.info("Clicked on new record")
+inputsql.readDataInputSQL(obj)
