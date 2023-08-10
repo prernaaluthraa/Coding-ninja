3e5a0cebdbb253adba9c7ea869a87ca7469694f8
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-08-10 03:22:56 -0400

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -1,46 +1,22 @@
-/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Framework/getTransaction.py:31: FutureWarning: The frame.append method is deprecated and will be removed from pandas in a future version. Use pandas.concat instead.
-  all_data=all_data.append(pd.read_excel(obj.config['M_IN']+file,sheet_name='Sheet1', engine ='openpyxl'))
-file found
-/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Framework/getTransaction.py:31: FutureWarning: The frame.append method is deprecated and will be removed from pandas in a future version. Use pandas.concat instead.
-  all_data=all_data.append(pd.read_excel(obj.config['M_IN']+file,sheet_name='Sheet1', engine ='openpyxl'))
-file found
-/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Framework/getTransaction.py:31: FutureWarning: The frame.append method is deprecated and will be removed from pandas in a future version. Use pandas.concat instead.
-  all_data=all_data.append(pd.read_excel(obj.config['M_IN']+file,sheet_name='Sheet1', engine ='openpyxl'))
-file found
-/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Framework/getTransaction.py:31: FutureWarning: The frame.append method is deprecated and will be removed from pandas in a future version. Use pandas.concat instead.
-  all_data=all_data.append(pd.read_excel(obj.config['M_IN']+file,sheet_name='Sheet1', engine ='openpyxl'))
-file found
-/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Framework/getTransaction.py:31: FutureWarning: The frame.append method is deprecated and will be removed from pandas in a future version. Use pandas.concat instead.
-  all_data=all_data.append(pd.read_excel(obj.config['M_IN']+file,sheet_name='Sheet1', engine ='openpyxl'))
-file found
-in catch of gtFile is not a zip file
+obj.driver.execute_script("window.scrollBy(-3000,0)")
+    try:
+        dropdown_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/div[2]/main/div/div/div[1]/div/div/div[2]/div/div/div/div[18]/div/a')))
+        dropdown_button.click()
+        print("PLS Investor settings clicked")
+
+Process error ('error', 'Message: unknown error: Element is not clickable at point (62, 957)\n  (Session info: headless chrome=110.0.5481.177)\n  (Driver info: chromedriver=2.40.565383 (76257d1ab79276b2d53ee976b2c3e3b9f335cde7),platform=Linux 3.10.0-1160.95.1.el7.x86_64 x86_64)\n')
 Traceback (most recent call last):
-  File "/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Finalmain.py", line 267, in main
-    GT.loadData(obj)
-  File "/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Framework/getTransaction.py", line 31, in loadData
-    all_data=all_data.append(pd.read_excel(obj.config['M_IN']+file,sheet_name='Sheet1', engine ='openpyxl'))
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/pandas/util/_decorators.py", line 211, in wrapper
-    return func(*args, **kwargs)
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/pandas/util/_decorators.py", line 331, in wrapper
-    return func(*args, **kwargs)
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/pandas/io/excel/_base.py", line 482, in read_excel
-    io = ExcelFile(io, storage_options=storage_options, engine=engine)
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/pandas/io/excel/_base.py", line 1695, in __init__
-    self._reader = self._engines[engine](self._io, storage_options=storage_options)
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/pandas/io/excel/_openpyxl.py", line 557, in __init__
-    super().__init__(filepath_or_buffer, storage_options=storage_options)
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/pandas/io/excel/_base.py", line 545, in __init__
-    self.book = self.load_workbook(self.handles.handle)
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/pandas/io/excel/_openpyxl.py", line 568, in load_workbook
-    return load_workbook(
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/openpyxl/reader/excel.py", line 315, in load_workbook
-    reader = ExcelReader(filename, read_only, keep_vba,
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/openpyxl/reader/excel.py", line 124, in __init__
-    self.archive = _validate_archive(fn)
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/openpyxl/reader/excel.py", line 96, in _validate_archive
-    archive = ZipFile(filename, 'r')
-  File "/usr/local/lib/python3.9/zipfile.py", line 1266, in __init__
-    self._RealGetContents()
-  File "/usr/local/lib/python3.9/zipfile.py", line 1333, in _RealGetContents
-    raise BadZipFile("File is not a zip file")
-zipfile.BadZipFile: File is not a zip file
+  File "/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Framework/PLSInvestorNewRecord.py", line 122, in searchInvestor
+    dropdown_button.click()
+  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/selenium/webdriver/remote/webelement.py", line 80, in click
+    self._execute(Command.CLICK_ELEMENT)
+  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/selenium/webdriver/remote/webelement.py", line 633, in _execute
+    return self._parent.execute(command, params)
+  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/selenium/webdriver/remote/webdriver.py", line 321, in execute
+    self.error_handler.check_response(response)
+  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/selenium/webdriver/remote/errorhandler.py", line 242, in check_response
+    raise exception_class(message, screen, stacktrace)
+selenium.common.exceptions.WebDriverException: Message: unknown error: Element is not clickable at point (62, 957)
+  (Session info: headless chrome=110.0.5481.177)
+  (Driver info: chromedriver=2.40.565383 (76257d1ab79276b2d53ee976b2c3e3b9f335cde7),platform=Linux 3.10.0-1160.95.1.el7.x86_64 x86_64)
+
