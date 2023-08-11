078e57f778081524990d5452c0bf72f426aedd50
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-08-11 00:52:38 -0400

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -1,80 +1,32 @@
-Process error ('error', '1')
-Traceback (most recent call last):
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/pandas/core/indexes/base.py", line 3803, in get_loc
-    return self._engine.get_loc(casted_key)
-  File "pandas/_libs/index.pyx", line 138, in pandas._libs.index.IndexEngine.get_loc
-  File "pandas/_libs/index.pyx", line 162, in pandas._libs.index.IndexEngine.get_loc
-  File "pandas/_libs/index.pyx", line 197, in pandas._libs.index.IndexEngine._get_loc_duplicates
-KeyError: 1
+    try:
+        mod_allowed = str(obj.mod_allowed)
+        mod_toggle = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'/html/body/div/div/div[2]/main/div/div/div[2]/div/div/div[3]/form/div[1]/div/div[2]/div/div[1]/div[2]/div/div/div/table/tbody/tr[1]/td[2]/div/div/div/div/label[2]/span')))
 
-The above exception was the direct cause of the following exception:
-
-Traceback (most recent call last):
-  File "/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Framework/PLSInvestorNewRecord.py", line 153, in searchInvestor
-    dropdown_field.send_keys(obj.INVESTOR_IDNumber)
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/selenium/webdriver/remote/webelement.py", line 478, in send_keys
-    {'text': "".join(keys_to_typing(value)),
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/selenium/webdriver/common/utils.py", line 151, in keys_to_typing
-    typing.append(val[i])
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/pandas/core/series.py", line 981, in __getitem__
-    return self._get_value(key)
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/pandas/core/series.py", line 1089, in _get_value
-    loc = self.index.get_loc(label)
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/pandas/core/indexes/base.py", line 3805, in get_loc
-    raise KeyError(key) from err
-KeyError: 1
-
-During handling of the above exception, another exception occurred:
+        if mod_allowed == "YES" :
+            mod_toggle.click()
+            obj.mod_allowed_updated = "YES"
+            print("Mod toggle selected")
+            obj.printlog.info("Mod toggle selected. input:{mod_allowed} 1")   
+        else:
+            
+            print("mod_toggle already toggle no")
+            obj.mod_allowed_updated = "NO"
+            obj.printlog.info("Mod toggle  already unselected. input:{mod_allowed} 1")
+        obj.printlog.info("Mod toggle answered. 4.3")
+        obj.driver.save_screenshot(obj.config['SS']+obj.INVESTOR_IDNumber+"Modtoggle_1.png")
+    except (TimeoutException, StaleElementReferenceException) as e:
+        traceback.print_exc()           
+        print(f"error:{e}")       
+    except Exception as ex:
+        raise Exception(f"error:", str(ex))
 
 Traceback (most recent call last):
-  File "/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Finalmain.py", line 340, in main
-    pr.inflightPrcs(obj)
-  File "/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Framework/process.py", line 32, in inflightPrcs
-    new.searchInvestor(obj)
-  File "/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Framework/PLSInvestorNewRecord.py", line 168, in searchInvestor
-    raise Exception(f"error", str(ex))
-Exception: ('error', '1')
-Restarting session
-'NoneType' object has no attribute 'terminate'
-Traceback (most recent call last):
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/pandas/core/indexes/base.py", line 3803, in get_loc
-    return self._engine.get_loc(casted_key)
-  File "pandas/_libs/index.pyx", line 138, in pandas._libs.index.IndexEngine.get_loc
-  File "pandas/_libs/index.pyx", line 162, in pandas._libs.index.IndexEngine.get_loc
-  File "pandas/_libs/index.pyx", line 197, in pandas._libs.index.IndexEngine._get_loc_duplicates
-KeyError: 1
-
-The above exception was the direct cause of the following exception:
-
-Traceback (most recent call last):
-  File "/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Framework/PLSInvestorNewRecord.py", line 153, in searchInvestor
-    dropdown_field.send_keys(obj.INVESTOR_IDNumber)
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/selenium/webdriver/remote/webelement.py", line 478, in send_keys
-    {'text': "".join(keys_to_typing(value)),
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/selenium/webdriver/common/utils.py", line 151, in keys_to_typing
-    typing.append(val[i])
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/pandas/core/series.py", line 981, in __getitem__
-    return self._get_value(key)
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/pandas/core/series.py", line 1089, in _get_value
-    loc = self.index.get_loc(label)
-  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/pandas/core/indexes/base.py", line 3805, in get_loc
-    raise KeyError(key) from err
-KeyError: 1
-
-During handling of the above exception, another exception occurred:
-
-Traceback (most recent call last):
-  File "/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Finalmain.py", line 340, in main
-    pr.inflightPrcs(obj)
-  File "/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Framework/process.py", line 32, in inflightPrcs
-    new.searchInvestor(obj)
-  File "/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Framework/PLSInvestorNewRecord.py", line 168, in searchInvestor
-    raise Exception(f"error", str(ex))
-Exception: ('error', '1')
-
-During handling of the above exception, another exception occurred:
-
-Traceback (most recent call last):
-  File "/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Finalmain.py", line 363, in main
-    obj.terminalConnection.terminate()
-AttributeError: 'NoneType' object has no attribute 'terminate'
+  File "/application/RPA/LRPU/PLSInvestorRulesAutomation/SRC/Framework/CreatePLSInvestor.py", line 37, in createNew
+    obj.driver.save_screenshot(obj.config['SS']+obj.INVESTOR_IDNumber+"Modtoggle_1.png")
+  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/selenium/webdriver/remote/webdriver.py", line 1055, in save_screenshot
+    return self.get_screenshot_as_file(filename)
+  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/selenium/webdriver/remote/webdriver.py", line 1029, in get_screenshot_as_file
+    if not filename.lower().endswith('.png'):
+  File "/home/svc_dl_dev_exec/.local/lib/python3.9/site-packages/pandas/core/generic.py", line 5902, in __getattr__
+    return object.__getattribute__(self, name)
+AttributeError: 'Series' object has no attribute 'lower'
