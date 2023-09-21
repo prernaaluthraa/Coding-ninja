c5f71fdc4ebcc563f0ac468e84de6eb42cee3bce
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-09-21 12:18:50 +0530

    Update Array4.java

--- Array4.java
+++ Array4.java
@@ -1,16 +1,17 @@
-def SaveRecord(obj):
-    save_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div/div/main/div/div[2]/div/div[2]/div/div/div[3]/form/div[2]/div[5]/button')))
-    save_button.click()
-	#Saving changes window
-    try:
-        updatereason = "Investor Data Updated by BOT"
-        reason = driver.find_element(By.XPATH, '/html/body/div[3]/div/div/form/div[2]/div/div/div/div[1]/div/textarea')
-        reason.send_keys(updatereason)
-        save_check = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div/div/form/div[3]/div/div[2]/button')))
-    	save_check.click()
-    except (TimeoutException, StaleElementReferenceException) as e:
-        traceback.print_exc()           
-        print(f"error:{e}") 
-        obj.printlog.info(f"error:{e}")      
-    except Exception as ex:
-        raise Exception(f"error:", str(ex))
+# Check files in the "deleted_files" folder
+for folder in os.listdir(temp_folder_path):
+    folder_path = os.path.join(temp_folder_path, folder)
+    if os.path.isdir(folder_path) and os.path.getmtime(folder_path) < seven_days_ago:
+        # Extract files from the ZIP folder
+        shutil.unpack_archive(folder_path, temp_folder_path)
+        
+        # Delete files in the extracted folder
+        for root, dirs, files in os.walk(temp_folder_path):
+            for file in files:
+                file_path = os.path.abspath(os.path.join(root, file))
+                if os.path.isfile(file_path):
+                    csvwriter.writerow(["Deleting file", file_path, ""])
+                    os.remove(file_path)
+        
+        # Delete the extracted folder
+        shutil.rmtree(folder_path)
