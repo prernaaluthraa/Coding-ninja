c036366f3abac687699f5f75435b78aa571ac306
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-12-29 08:39:35 -0500

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -1,2 +1,14 @@
 Throughout the past year, my commitment to excellence has been evident across diverse organizational functions. Notably, I have maintained a flawless record in completing all assigned trainings and employee obligations on time, achieving a 100% completion rate and demonstrating proactive time management skills. In L1 activities, my precision and diligence resulted in a perfect 100% success rate in code migration, and my strategic recommendations led to a remarkable 50% reduction in failures. As a leader in development, I successfully led the end-to-end execution of Project Romeo - BKFS PLS Investor Rules, showcasing technical proficiency, while seamlessly transitioning to lead the BKFS Final Mod project, implementing enhancements based on Business Units' requirements. My involvement in Experimentation initiatives included innovative use of bots and servers, automation of scripts for efficient server storage management, and repurposing vacant bots for the deployment of Project Romeo. In customer service, my dedication is reflected in consistent high Net Promoter Scores, proactive updates to processes, and reconfigurations based on valuable feedback, ensuring clear communication and client satisfaction. These multifaceted contributions underscore my adaptability, technical expertise, and proactive leadership, positioning me as a valuable asset contributing to the organization's success and client satisfaction.
- 
+
+
+Evaluate Topic Leadership Behaviors and Risk & Compliance
+
+1.1. Behaves as a Leadership Role Model
+1.2. Builds Strong Working Relationships
+1.3. Promotes Diversity, Equity & Inclusion
+1.4. Drives and Delivers Exceptional Results
+1.5. Embraces Service Excellence
+2.1. Comply with regulations detailed in the policies and procedures for your role
+2.2. Identify, address and escalate compliance and risk issues as appropriate within the scope of your role
+2.3. Completion of required compliance training
+
