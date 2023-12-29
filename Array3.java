bb8317fec2a7f0f9042b8e6e8489a0e312789134
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-12-29 09:02:00 -0500

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -2,3 +2,14 @@ In assessing Leadership Behaviors and Risk & Compliance, my performance has cons
 
 In the realm of Risk & Compliance, I adhere rigorously to regulations outlined in policies and procedures. Identifying, addressing, and appropriately escalating compliance and risk issues is integral to my role, ensuring a proactive approach to risk management. Additionally, I prioritize the completion of required compliance training, staying abreast of industry standards and best practices to maintain a high level of proficiency in this critical area. Overall, my performance reflects a commitment to leadership excellence and a thorough understanding and application of risk and compliance principles.
   
+Throughout the past year, my commitment to excellence has been evident across diverse organizational functions. Notably, I have maintained a flawless record in completing all assigned trainings and employee obligations on time, achieving a 100% completion rate and demonstrating proactive time management skills. 
+In L1 activities, my precision and diligence resulted in a perfect success rate in code migration, and my strategic recommendations led to a remarkable 50% reduction in failures. 
+In development, I successfully led the end-to-end execution of Project Romeo - BKFS PLS Investor Rules, showcasing technical proficiency, while seamlessly transitioning to lead the BKFS Final Mod project, implementing enhancements based on Business Units' requirements. 
+From time to time, I have also been efficiently able to make and test changes in the code as per Business Units requirements. Be it the case for BKFS interface change or any changes in the mapping document, I have always been able to deliver the required results on time and as per the requirement.
+I have also been able to help the developers in testing and making updates in case of enhancements and interface changes to any process. 
+My involvement in Experimentation initiatives included innovative use of bots and servers, developing automation scripts in python for efficient server storage management, and repurposing vacant bots for the deployment of Project Romeo or the Reverse Servicing project. 
+In customer service, my dedication is reflected in the team performance, providing proactive updates to processes, and reconfigurations of bots based on valuable feedback, ensuring clear communication and BU's satisfaction by providing them timely status and the API key access to independently control the process executions.
+These multifaceted contributions underscore my adaptability, technical expertise, and proactive leadership, positioning me as a valuable asset contributing to the organization's success and client satisfaction.
+ 
+
+Leadership Behaviors and Risk & Compliance
