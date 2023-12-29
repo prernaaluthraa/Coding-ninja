fc39e91e0c607438ba2d741fc1b5a76d4a41e6db
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-12-29 07:53:58 -0500

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -1,2 +1,2 @@
-I take immense pride in excelling at customer service, consistently providing exceptional support to Business Units (BU's). I actively engage with them through regular meetings, ensuring a deep understanding of their needs. I empower them by providing necessary tools and reports, fostering independence. My commitment to maintaining a high Net Promoter Score (NPS) is reflected in my proactive approach to changing processes and reconfiguring systems based on valuable feedback. I prioritize timely responses to Business Units, ensuring clear and effective communication between teams. My dedication to superior customer service has consistently positioned our team at the forefront of client satisfaction within the organization.
-  s
+L1 activities
+Code migration / Job monitoring & taking necessary actions in case of failures / Recommending developers for improving the code fixes to reduce the number of failures in production environment
