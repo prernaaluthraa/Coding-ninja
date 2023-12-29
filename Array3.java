f70361e32a3f61f0d798c683630474d3cdb29a63
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2023-12-29 19:37:42 +0530

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -1,15 +1,16 @@
-In assessing Leadership Behaviors and Risk & Compliance, my performance has consistently demonstrated a commitment to excellence across key dimensions. As a leadership role model, I've exhibited behaviors that inspire and guide the team, setting high standards for professionalism. Building strong working relationships has been a priority, fostering collaboration and effective communication. My dedication to promoting diversity, equity, and inclusion is evident in my proactive efforts to create an inclusive work environment. In driving and delivering exceptional results, I have consistently met or exceeded performance expectations, contributing to the overall success of the team. Embracing service excellence, I strive to deliver the highest quality in every aspect of my work.
+In my leadership role, I consistently exhibit excellence across key dimensions, serving as a model for high standards of professionalism. I prioritize building strong working relationships, fostering collaboration, and promoting diversity, equity, and inclusion to create an inclusive work environment. My commitment to delivering exceptional results is evident in consistently meeting or exceeding performance expectations, contributing to overall team success. I embrace service excellence, striving for the highest quality in every aspect of my work.
 
-In the realm of Risk & Compliance, I adhere rigorously to regulations outlined in policies and procedures. Identifying, addressing, and appropriately escalating compliance and risk issues is integral to my role, ensuring a proactive approach to risk management. Additionally, I prioritize the completion of required compliance training, staying abreast of industry standards and best practices to maintain a high level of proficiency in this critical area. Overall, my performance reflects a commitment to leadership excellence and a thorough understanding and application of risk and compliance principles.
-  
-Throughout the past year, my commitment to excellence has been evident across diverse organizational functions. Notably, I have maintained a flawless record in completing all assigned trainings and employee obligations on time, achieving a 100% completion rate and demonstrating proactive time management skills. 
-In L1 activities, my precision and diligence resulted in a perfect success rate in code migration, and my strategic recommendations led to a remarkable 50% reduction in failures. 
-In development, I successfully led the end-to-end execution of Project Romeo - BKFS PLS Investor Rules, showcasing technical proficiency, while seamlessly transitioning to lead the BKFS Final Mod project, implementing enhancements based on Business Units' requirements. 
-From time to time, I have also been efficiently able to make and test changes in the code as per Business Units requirements. Be it the case for BKFS interface change or any changes in the mapping document, I have always been able to deliver the required results on time and as per the requirement.
-I have also been able to help the developers in testing and making updates in case of enhancements and interface changes to any process. 
-My involvement in Experimentation initiatives included innovative use of bots and servers, developing automation scripts in python for efficient server storage management, and repurposing vacant bots for the deployment of Project Romeo or the Reverse Servicing project. 
-In customer service, my dedication is reflected in the team performance, providing proactive updates to processes, and reconfigurations of bots based on valuable feedback, ensuring clear communication and BU's satisfaction by providing them timely status and the API key access to independently control the process executions.
-These multifaceted contributions underscore my adaptability, technical expertise, and proactive leadership, positioning me as a valuable asset contributing to the organization's success and client satisfaction.
- 
+In Risk & Compliance, I rigorously adhere to regulations, identify and address issues proactively, and escalate them appropriately, demonstrating a thorough understanding and application of risk and compliance principles. I prioritize compliance training to stay updated on industry standards and best practices, maintaining a high level of proficiency in this critical area.
 
-Leadership Behaviors and Risk & Compliance
+Over the past year, my commitment to excellence is reflected in maintaining a flawless record in completing all assigned trainings and obligations on time, achieving a 100% completion rate. In L1 activities, my precision and diligence resulted in a perfect success rate in code migration, with strategic recommendations leading to a remarkable 50% reduction in failures.
+
+In development, I successfully led the execution of Project Romeo - BKFS PLS Investor Rules and seamlessly transitioned to lead the BKFS Final Mod project, implementing enhancements based on Business Units' requirements. I efficiently make and test changes in the code as per Business Units requirements, ensuring timely delivery of required results.
+
+My involvement in Experimentation initiatives showcases my adaptability, technical expertise, and proactive leadership. I contribute to customer service by providing proactive updates, reconfigurations of bots, and clear communication, ensuring client satisfaction and contributing to the organization's success.
+
+These multifaceted contributions underscore my value as a valuable asset to the organization, positioning me as a key contributor to client satisfaction and overall success.
+
+
+
+
+User
