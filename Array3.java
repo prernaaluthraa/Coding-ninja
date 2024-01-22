eeb84875214af9ef45791556b811c03b9e040a37
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2024-01-22 15:34:16 -0500

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -1,30 +1,19 @@
-Open Terminal Session:
+if(String.IsNullOrEmpty(in_TransactionItem("PTFCAP_AMOUNT").ToString.Trim),"0",in_TransactionItem("PTFCAP_AMOUNT").ToString.Trim)
 
-Use the "Attach Terminal" activity:
-Set the ConnectionType property to the appropriate terminal type.
-Configure the ConnectionString property to provide connection details or indicate the terminal window.
-Navigate to Summary of Plans in Last 24 Months Header:
+FHA
+"Update Plan Terms > Navigate to Plan Terms Header > Refer Do you want to add an additional month to the Workout Effective Date? field >
+Go to  to BIS Input File > Navigate to column name MED and Mod Proccessing Pad > consider the date available below these column and perform below steps to check if Slider needs to move towards "" Yes ""  or "" No ""
+-- If the days difference between  MED and Mod Proccessing Pad column is less than 30 days or negative > than move the slider towards "" Yes "" otherwise don't move the slider i.e it should be towards "" No "" and procced to next step
+For Example
+Med Date - 2/1/2024 (-) Mod Proccessing Pad - 11/09/2023 = Days Difference =  84 ( No need to move slider it should be towards "" No "" )
+Med Date - 2/1/2024 (-) Mod Proccessing Pad - 01/03/2024 = Days Difference =  29 ( Move the slider towards "" Yes "" )
+Med Date - 2/1/2024 (-) Mod Proccessing Pad - 02/02/2024 = Days Difference =  -1 ( Move the slider towards "" Yes "" )"
 
-Use activities like "Send Control Key" or "Type Into":
-Send appropriate keys or commands to navigate to the "Summary of Plans in Last 24 Months" section.
-Check if KEPT is Reflecting:
 
-Use the "Get Text" activity:
-Set the Selector property to indicate the third column below the header.
-Assign the extracted text to a variable (ExtractedText).
-Use a condition or string manipulation to check if "KEPT" is present in ExtractedText.
-Extract Dates for KEPT and Other Statuses:
-
-Use multiple "Get Text" activities:
-Set selectors to extract dates for "Broken," "Deleted," "Changes," "Expired," and "KEPT" from the third column below the header.
-Assign each extracted date to a corresponding variable (e.g., BrokenDate, DeletedDate, etc.).
-Compare Dates:
-
-Use programming constructs (e.g., If statements):
-Compare the dates using conditions. You might need to convert the date strings to DateTime objects for accurate comparison.
-Decide whether the date associated with "KEPT" is more recent than the other statuses.
-Handle Exception if KEPT Date is Older:
-
-Use the "Log Message" activity or "Throw" activity:
-If the date associated with "KEPT" is older than the other statuses, log an exception or use the "Throw" activity to stop the execution.
-Here's
+FHLMC-FNMA
+"If Workout Plan fields reflects Flex Modification (SMDU) - No action required from BOT
+If Workout Plan field reflects Flex Modification (Resolve) or FNMA Non-SMDU - than follow below
+Update Plan Terms -
+Navigate to Plan Terms Header > Refer Do you want to add an additional month to the Workout Effective Date? field > GO to BIS input file > Navigate to column name LAST TRIAL PAYMENT MADE DATE and Column name Trial Due dates end month consider the date available below these column and perform below steps to check if Slider needs to move towards "" Yes ""  or "" No ""
+-- Compare the last trial payment made date with Trial due end month if the date below last trial payment made is greater than the trial due date end month than move the slider towards "" Yes "" 
+--Compare the last trial payment made date with Trial due end month if the date below last trial payment made is less than Trial due end month  don't move the slider i.e it should be towards "" No "" and  procced to next step"
