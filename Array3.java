f17a32c7a74e28da90f7d078f6aeaaa9a99b292d
Prerna Luthra <58359868+prernaaluthraa@users.noreply.github.com>
2024-01-24 04:53:20 -0500

    Update Array3.java

--- Array3.java
+++ Array3.java
@@ -1,30 +1,8 @@
-"Go to Director >P2> Enter Loan No in Loan No field > Hit Enter
-Go to LMT3 screen > Under STEP field > Check for M89 
-Navigate to ACT field of M89 > Type C"" in box under ACT field of M89
-Navigate to ACTUAL field of M89 > Type BOT Review Date MMDDYY > Press Enter
-Type ( FINAL MODIFICATION CALCULATED ) in box line against M89 ACT field > Press Enter and follow below basis on Workout Plan"
-"i. If Workout Plan = Standalone Partial Claim 
-Go to LMT3 screen > Under STEP field > Check for L61 
-Navigate to ACT field of L61 > Type C"" in box under ACT field of L61
-Navigate to ACTUAL field of L61 > Type BOT Review Date MMDDYY > Press Enter
-Type ( FINAL MODIFICATION CALCULATED ) in box line against L61 ACT field > Press Enter"
-"ii. If Workout Plan = Recovery without PC – 30 Year
-Go to LMT3 screen > Under STEP field > Check for L62 
-Navigate to ACT field of L62 > Type C"" in box under ACT field of L62
-Navigate to ACTUAL field of L62 > Type BOT Review Date MMDDYY > Press Enter
-Type ( FINAL MODIFICATION CALCULATED ) in box line against L62 ACT field > Press Enter"
-"iii. If Workout Plan = Recovery with PC – 30 Year
-Go to LMT3 screen > Under STEP field > Check for L62 
-Navigate to ACT field of L62 and L67  > Type C"" in box under ACT field of L62 and L67
-Navigate to ACTUAL field of L62 and L67 > Type BOT Review Date MMDDYY > Press Enter
-Type ( FINAL MODIFICATION CALCULATED ) in box line against L62 and L67 ACT field > Press Enter"
-"iv. If Workout Plan = Recovery without PC – 40 Year
-Go to LMT3 screen > Under STEP field > Check for L63
-Navigate to ACT field of L63  > Type C"" in box under ACT field of L63
-Navigate to ACTUAL field of L63 > Type BOT Review Date MMDDYY > Press Enter
-Type ( FINAL MODIFICATION CALCULATED ) in box line against L63 ACT field > Press Enter"
-"v. If Workout Plan = Recovery with PC – 40 Year
-Go to LMT3 screen > Under STEP field > Check for L63 and L67
-Navigate to ACT field of L63 and L67  > Type C"" in box under ACT field of L63 and L67
-Navigate to ACTUAL field of L63 and L67 > Type BOT Review Date MMDDYY > Press Enter
-Type ( FINAL MODIFICATION CALCULATED ) in box line against L63 and L67 ACT field > Press Enter"
+function getValue(element,inputElement) 
+{
+myDivObj = document.querySelector(inputElement).checked
+return myDivObj
+}
+
+input parameter
+"input[id='manualInput.additionalInterimMonthIndicator']"
